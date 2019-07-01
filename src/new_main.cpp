a83e866d8235a9c5bb40e0b2dea832c76255a922
Ryan Jung <chimin.ryan.jung@gmail.com>
2019-07-01 14:29:58 -0400

    test10

--- src/new_main.cpp
+++ src/new_main.cpp
@@ -145,14 +145,14 @@ void thread_method(const char *pairs_filename, vector<Trip_Request> trips, Plan 
     float distance = 0.0;
 
     plan.path.clear();
+    cout << "working" << endl;
     event_handler.clear();
     double time_elapsed;
 
     router.find_path((Algorithm)algorithm, request_handler.request(), plan,
                      time_elapsed, trip_request.nfaID);
 
-    cout << "working" << endl;
-    mtx.lock();
+        mtx.lock();
     out_file << trip_request.id << '\t'
              << trip_request.source << '\t'
              << trip_request.destination << '\t';
