7211bdbc0b38256fee01f5f0afb5788148d3e7f7
Ryan Jung <chimin.ryan.jung@gmail.com>
2019-07-15 12:56:28 -0400

    more stuff

--- example/testing_frame.py
+++ example/testing_frame.py
@@ -12,24 +12,18 @@ from statistics import mean
 
 def write_test():
     f = open("test-trip-file.txt", "+w")
-    print("flag1")
-    node_num1 = random.randint(1,10)
-    print("flag2")
-    node_num2 = random.randint(1,10)
-    print("flag3")
-    while(node_num2 == node_num1):
-        node_num2 = random.randint(1,10)
-    print("flag4")
     for i in range(100):
+        node_num1 = random.randint(1,10)
+        node_num2 = random.randint(1,10)
+        while(node_num2 == node_num1):
+            node_num2 = random.randint(1,10)
         f.write(str(i) + " " + str(node_num1) + " " + str(node_num2) + " " + str(10) + " " + str(0) + "\n")
-    print("flag5")
     f.close
-    print("flag6")
 
 def subprocess_cmd(command): #Adapted from stackoverflow example
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
-    print(proc_stdout)
+    #print(proc_stdout)
 
 write_test()
 subprocess_cmd("cd ../src; make; cd ../example;")
