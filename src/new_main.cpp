d5250e97b89d71bab9e61e7846257d32c7bd1c31
Ryan Jung <chimin.ryan.jung@gmail.com>
2019-07-22 11:14:51 -0400

    tests

--- src/new_main.cpp
+++ src/new_main.cpp
@@ -80,13 +80,13 @@ void print_usage()
 //Threaded trip request processing
 void thread_method(const char *pairs_filename, vector<Trip_Request> trips, Plan plan, Network_Graph &network, unsigned int algorithm, ostream &out_file, int singleNFA, string nfa_filename, const char *nfa_collection_filename)
 {
-  mtx.lock();
+  /* mtx.lock();
   string glob_string = string(1, glob);
   glob_string = glob_string + "_out.txt";
   ofstream read_out(glob_string);
   //read_out.open(glob_string, std::fstream::in);
   glob++;
-  mtx.unlock();
+  mtx.unlock();*/
   Request_Handler request_handler;
   request_handler.set_mode(FILE_PAIRS);
   request_handler.set_stream(pairs_filename);
@@ -115,34 +115,34 @@ void thread_method(const char *pairs_filename, vector<Trip_Request> trips, Plan 
 
     file >> nNFAs;
     // cout << "Found " << nNFAs << " NFAs" << endl;
-    read_out << "Found " << nNFAs << " NFAs" << endl;
+    //read_out << "Found " << nNFAs << " NFAs" << endl;
     file.get();
 
     for (unsigned int i = 0; (int)i < nNFAs; ++i)
     {
       file.getline(buffer, 5000, '\n');
       //cout << "NFA: " << i << "\t" << buffer << endl;
-      read_out << "NFA: " << i << "\t" << buffer << endl;
+      //read_out << "NFA: " << i << "\t" << buffer << endl;
       NFA_Graph *nfa = new NFA_Graph(buffer, network);
       nfaVector.push_back(nfa);
     }
   }
 
   //cout << "Status." << endl;
-  read_out << "Status." << endl;
+  //read_out << "Status." << endl;
 
   LOG4CPLUS_DEBUG(main_logger, "Building router...");
   Router router(network, nfaVector);
   LOG4CPLUS_DEBUG(main_logger, "Router built.");
 
   //cout << "Status.." << endl;
-  read_out << "Status.." << endl;
+  //read_out << "Status.." << endl;
 
-  event_handler.set_graph(network);
+  //event_handler.set_graph(network);
 
   //cout << "Status..." << endl;
 
-  read_out << "Status..." << endl;
+  //read_out << "Status..." << endl;
 
   // initialize request handler
 
@@ -152,7 +152,7 @@ void thread_method(const char *pairs_filename, vector<Trip_Request> trips, Plan 
   //put a thing here -----------------------v change  that
   vector<Trip_Request> trip_list = trips;
   //cout << "Status...." << endl;
-  read_out << "Status...." << endl;
+  //read_out << "Status...." << endl;
 
   while (!trip_list.empty())
   {
@@ -187,7 +187,7 @@ void thread_method(const char *pairs_filename, vector<Trip_Request> trips, Plan 
       LOG4CPLUS_ERROR(main_logger, error_message);
 
     trip_list.pop_back();
-    read_out.close();
+    //read_out.close();
   }
   /* while (!request_handler.finished())
   {
@@ -294,7 +294,7 @@ int main(int argc, char *argv[])
   // build network
   LOG4CPLUS_DEBUG(main_logger, "Building network...");
   Network_Graph network(network_filename, coords_filename);
-
+  event_handler.set_graph(network);
   LOG4CPLUS_DEBUG(main_logger, "Building NFA...");
 
   //splitting everything up into threads
