988abf2ba7e4b1639392384f71c6407424381701
Ryan Jung <chimin.ryan.jung@gmail.com>
2019-07-24 14:07:09 -0400

    scripting

--- example/data_script.sh
+++ example/data_script.sh
@@ -1,27 +1,13 @@
-avg_time() {
-    #
-    # usage: avg_time n command ...
-    #
-    n=$1; shift
-    (($# > 0)) || return                   # bail if no command given
-    for ((i = 0; i < n; i++)); do
-        { time -p "$@" &>/dev/null; } 2>&1 # ignore the output of the command
-                                           # but collect time's output in stdout
-    done | awk '
-        /real/ { real = real + $2; nr++ }
-        /user/ { user = user + $2; nu++ }
-        /sys/  { sys  = sys  + $2; ns++}
-        END    {
-                 if (nr>0) printf("real %f\n", real/nr);
-                 if (nu>0) printf("user %f\n", user/nu);
-                 if (ns>0) printf("sys %f\n",  sys/ns)
-               }'
-}
+#some parts of this script were adapted from internet sources
 
 cd ../src
 make
 cd ../example
 ./trip_maker.py
 #avg_time 100 ../src/new_main -g network-links.txt -c network-nodes.txt -N nfa_main.txt -t 12 -f test-trip-file.txt
-time for i in {1..10}; do ../src/new_main -g network-links.txt -c network-nodes.txt -N nfa_main.txt -t 12 -f test-trip-file.txt ; done
+start=$(date +%s)
+time for i in {1..100}; do ../src/new_main -g network-links.txt -c network-nodes.txt -N nfa_main.txt -t 12 -f test-trip-file.txt ; done
+end=$(date +%s)
+echo average time:
+bc -l <<< "($end -$start) / 10" | sed 's/[0]*$//g'
 
