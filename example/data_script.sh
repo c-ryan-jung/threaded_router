b2d76629603f32b4a50eb436465b971ebf41c520
Ryan Jung <chimin.ryan.jung@gmail.com>
2019-07-24 13:01:27 -0400

    scripting fixes

--- example/data_script.sh
+++ example/data_script.sh
@@ -1,34 +1,20 @@
-avgByProc() { 
-    local foo start end n=$1 e=$1 values times
-    shift;
-    export n;
-    { 
-        read foo;
-        read foo;
-        read foo foo start foo
-    } < /proc/timer_list;
-    mapfile values < <(
-        for((;n--;)){ "$@" &>/dev/null;}
-        read -a endstat < /proc/self/stat
-        {
-            read foo
-            read foo
-            read foo foo end foo
-        } </proc/timer_list
-        printf -v times "%s/100/$e;" ${endstat[@]:13:4}
-        bc -l <<<"$[end-start]/10^9/$e;$times"
+avg_time_alt() { 
+    local -i n=$1
+    local foo real sys user
+    shift
+    (($# > 0)) || return;
+    { read foo real; read foo user; read foo sys ;} < <(
+        { time -p for((;n--;)){ "$@" &>/dev/null ;} ;} 2>&1
     )
-    printf -v fmt "%-7s: %%.5f\\n" real utime stime cutime cstime
-    printf "$fmt" ${values[@]}
+    printf "real: %.5f\nuser: %.5f\nsys : %.5f\n" $(
+        bc -l <<<"$real/$n;$user/$n;$sys/$n;" )
 }
 
 cd ../src
 make
 cd ../example
 ./trip_maker.py
-echo How many trials? 
-read varname
 
-avgByProc varname ../src/new_main -g network-links.txt -c network-nodes.txt -N nfa_main.txt -t 12 -f test-trip-file.txt
+avg_time_alt 3 ../src/new_main -g network-links.txt -c network-nodes.txt -N nfa_main.txt -t 12 -f test-trip-file.txt
 #time for i in {1..$varname}; do ../src/new_main -g network-links.txt -c network-nodes.txt -N nfa_main.txt -t 12 -f test-trip-file.txt ; done
 
