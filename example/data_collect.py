cc340537d2758a32e54e2b188859b665b3aed04b
Ryan Jung <chimin.ryan.jung@gmail.com>
2019-07-25 10:54:02 -0400

    features

--- example/data_collect.py
+++ example/data_collect.py
@@ -42,11 +42,12 @@ with open('test_data.csv', 'w') as f1:
             test = time.time() - start
             result_list.append(test)
         
-            #total_list.append(sum(result_list))
+        total_list.append(sum(result_list))
         avg_list.append(mean(result_list))
         print("Average time of " + str(core) + " Core(s):" + str(mean(result_list)) + " secs.")
         master_list.append(result_list)
-    writer.writerow(["Averages"] + avg_list)
+    
     writer.writerows(zip(*master_list))
-#print("Total time: " + str(sum(total_list)) + " secs")
+    writer.writerow(["Averages"] + avg_list)
+print("Total time: " + str(sum(total_list)) + " secs")
 exit()
