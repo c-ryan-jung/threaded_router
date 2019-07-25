0ca78581986d9b902bb393c547664b84a43aec2c
Ryan Jung <chimin.ryan.jung@gmail.com>
2019-07-25 11:05:07 -0400

    adjustment

--- example/data_collect.py
+++ example/data_collect.py
@@ -32,7 +32,7 @@ print("Trials: 100")
 print("Trip Requests: 1000")
 with open('test_data.csv', 'w') as f1:
     writer = csv.writer(f1, delimiter='\t',lineterminator='\n',)
-    writer.writerow(["1 Core"] + ["2 Cores"] + ["4 Cores"] + ["8 Cores"])
+    writer.writerow(["1 Core", "2 Cores", "4 Cores", "8 Cores"])
     for core in core_c:
         result_list = []
         for j in range(100):
