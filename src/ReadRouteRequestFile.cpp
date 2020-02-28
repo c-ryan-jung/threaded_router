2e9c1fdbda47cecb68153013639d914c861c3870
Henning S Mortveit <Henning.Mortveit@virginia.edu>
2020-02-27 22:33:08 -0500

    Updated testing example.

--- src/ReadRouteRequestFile.cpp
+++ src/ReadRouteRequestFile.cpp
@@ -9,44 +9,54 @@
 using namespace std;
 
 bool fileCheck(const char* filename){
-    struct stat buffer;
-    return (stat (filename, &buffer) == 0); 
+  if ( filename == 0 )
+    return false;
+
+  struct stat buffer;
+  return (stat (filename, &buffer) == 0) ? true : false;
 }
 
-unsigned int ReadRouteRequestPairs(const char* filename, vector<string_pair>& request_name_vector){
-    ifstream file(filename);
-    
-    string line;
-    string word;
-    vector<string> things;
-    bool first = true;
-    if(!file.is_open()){
-      cout << "There was an error opening the file of Request Pairs." << endl;
-      cout << "Filename given: " << filename << endl;
+unsigned int ReadRouteRequestPairs(const char* filename,
+				   vector<string_pair>& request_name_vector){
+  if ( filename == 0 )
+    return 1;
+
+  ifstream file(filename);
+
+  string line;
+  string word;
+  vector<string> things;
+  bool first = true;
+  if( !file.is_open() || file.bad() ){
+    cout << "\tReadRouteRequestPairs: Error opening file <"
+	 << filename << "> line: " << __LINE__ << endl;
+    return 1;
+  }
+
+  while(getline(file, line)){
+    if(first){
+      first = false;
+      continue;
+    }
+    istringstream myline(line);
+    while(myline.good()){
+      string substr;
+      getline(myline,substr, ',');
+      things.push_back(substr);
+    }
+  }
+  for(int i = 0; i < things.size(); i+=2){
+    if(fileCheck(things[i].c_str())){
+      //HSM: why use printf?
+      //printf("%s",things[i].c_str()); //for testing purposes
+      //printf("%s",things[i+1].c_str());
+      string_pair newPair(things[i],things[i+1]);
+      request_name_vector.push_back(newPair);
+    }else{
+      cout<<"\tReadRouteRequestPairs: Non-existing file specified or invalid entry. <"
+	  << things[i].c_str() << ">" << endl;
       return 1;
     }
-    while(getline(file, line)){
-        if(first){
-            first = false;
-            continue;
-        }
-        istringstream myline(line);
-        while(myline.good()){
-            string substr;
-            getline(myline,substr, ',');
-            things.push_back(substr);
-        }
-    }
-    for(int i = 0; i < things.size(); i+=2){
-        if(fileCheck(things[i].c_str())){
-            printf("%s",things[i].c_str()); //for testing purposes
-            printf("%s",things[i+1].c_str());
-            string_pair newPair(things[i],things[i+1]);
-            request_name_vector.push_back(newPair);
-        }else{
-            cout<<"Error bad files given"<<endl;
-            return 1;
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
--- src/test/RRRFtest.cpp
+++ src/test/RRRFtest.cpp
@@ -1,43 +1,79 @@
-#include "ReadRouteRequestFile.hpp"
+#include "../ReadRouteRequestFile.hpp"
 #include <iostream>
 
 using namespace std;
 
-typedef std::pair <std::string,std::string> string_pair;
+//typedef std::pair <std::string,std::string> string_pair;
 
-void test1(){
-    cout<<"Testing Basic function: ";
-    vector<string_pair> request_name_vector;
-    if(ReadRouteRequestPairs("testFile.txt", request_name_vector) == 0){
-        cout<<"Test Succesful"<<endl;
-    }else{
-        cout<<"Test Failed"<<endl;
-    }
-}
-void test2(){
-    cout<<"Testing file not exist: "<<endl;
-    vector<string_pair> request_name_vector;
-    if(ReadRouteRequestPairs("DoesNotExist.txt", request_name_vector) == 1){
-        cout<<"Test Succesful"<<endl;
-    }else{
-        cout<<"Test Failed"<<endl;
-    }
-}
-void test3(){
-    cout<<"Testing bad file pair check: "<<endl;
-    vector<string_pair> request_name_vector;
-    if(ReadRouteRequestPairs("badFile.txt", request_name_vector) == 1){
-        cout<<"Test Succesful"<<endl;
-    }else{
-        cout<<"Test Failed"<<endl;
-    }
+int test1(const char* filename = "testFile.txt"){
+  cout<<"Test1: file <" << filename << ">:" << endl;
 
-}
-
-int main(){
-    test1();
-    test2();
-    test3();
+  vector<string_pair> request_name_vector;
+  if(ReadRouteRequestPairs(filename, request_name_vector) == 0){
+    cout << "\tSuccess" <<endl;
     return 0;
+  }else{
+    cout<<"\tFail"<<endl;
+    return 1;
+  }
 }
 
+int test2(const char* filename = "DoesNotExist.txt"){
+  cout<<"Test2 [file exists]: file <" << filename << ">:" << endl;
+
+  vector<string_pair> request_name_vector;
+  if(ReadRouteRequestPairs(filename, request_name_vector) == 0){
+    cout<<"\tSuccess"<<endl;
+    return 0;
+  }else{
+    cout<<"\tFail"<<endl;
+    return 1;
+  }
+}
+
+int test3(const char* filename = "badFile.txt"){
+  cout<<"Test3 [bad pair]: file <" << filename << ">:" << endl;
+
+  vector<string_pair> request_name_vector;
+  if(ReadRouteRequestPairs("badFile.txt", request_name_vector) == 0){
+    cout<<"\tSuccess"<<endl;
+    return 0;
+  }else{
+    cout<<"\tFail"<<endl;
+    return 1;
+  }
+
+}
+
+int main(int argc, const char* argv[]){
+  if( argc < 2 ){
+    cout << "Missing argument(s)." << endl;
+    return 1;
+  }
+
+  unsigned int testVector[4] = {0};
+  unsigned int answerVector[4] = {1,1,1,1};
+
+  const char* filename = argv[1];
+
+  testVector[1] = test1(filename);
+  testVector[2] = test2();
+  testVector[3] = test3();
+
+  unsigned int numFails = 0;
+  for(int i=1; i<4; ++i){
+    cout << i << ' ' << testVector[i] << ' ' << answerVector[i] << endl;
+    if( testVector[i] != answerVector[i] )
+      numFails+=1;
+  }
+
+  if( numFails > 0 ){
+    cout << "One or more tests failed" << endl;
+    return 1;
+  }
+  else {
+    cout << "All tests passed" << endl;
+    return 0;
+  }
+
+}
